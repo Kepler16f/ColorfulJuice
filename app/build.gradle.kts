09a95ebe3f992d16e2bce004773c11644359cf9e
Kepler16f <Kepler16f@users.noreply.github.com>
2026-09-18 10:38:55 +0800

    style: 统一 widget 刷新按钮与设备名称色调; release 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.colorfuljuice.bluetoothbattery"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.2_dev2"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     buildTypes {
